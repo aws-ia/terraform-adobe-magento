1467a1047536c8da7992a322cf6f271d771b3b4a
Tony Vattathil <avattathil@gmail.com>
2021-09-13 13:30:49 -0700

    Add baseline VPC from aws-ia via terraform reg

--- deploy/main.tf
+++ deploy/main.tf
@@ -5,3 +5,17 @@ terraform {
   backend "remote" {}
   required_version = ">= 1.0.5"
 }
+
+# Use baseline VPC from aws-ia registery
+module "ecs_vpc" {
+  source               = "aws-ia/vpc/aws"
+  version              = "0.0.3"
+  name                 = "magento-vpc"
+  region               = var.region
+  cidr                 = "10.0.0.0/16"
+  public_subnets       = ["10.0.0.0/20"]
+  private_subnets_A    = ["10.0.16.0/20", "10.0.32.0/20", "10.0.48.0/20"]
+  enable_dns_hostnames = true
+  tags                 = {}
+  create_vpc           = true
+}
