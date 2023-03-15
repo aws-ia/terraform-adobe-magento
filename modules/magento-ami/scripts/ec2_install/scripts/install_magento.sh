44c463607ac0dae50ff23948a1291537f52e3e4f
pmcmanaman <pmcmanaman@users.noreply.github.com>
2023-03-15 14:12:49 -0400

    add AWS fraud and personalize extentions

--- modules/magento-ami/scripts/ec2_install/scripts/install_magento.sh
+++ modules/magento-ami/scripts/ec2_install/scripts/install_magento.sh
@@ -7,4 +7,7 @@ mkdir -p /home/magento/.config/composer/
 sudo mv $BASEDIR/configs/auth.json /home/magento/.config/composer/
 sudo chown -R magento. /home/magento/.config/
 cd /home/magento
-sudo -u magento composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition /var/www/html/magento -n
+sudo -u magento composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=2.4.5-p2 /var/www/html/magento -n
+cd /var/www/html/magento
+sudo -u magento composer require customerparadigm/amazon-personalize-extension:"^1.0"
+sudo -u magento composer require imaginationmedia/aws-fraud-magento2
